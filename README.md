66644722713997830207266a892ddd161594ff52
Shaun Hoyes <jshoyes@gmail.com>
2017-09-15 11:44:47 -0600

    snapshot of all active margin loans

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # poloniex-monitor
 externally manage Poloniex exchange account activity
+
+be sure to use the `constants-template.js` file as a guide to store your global variables. This should be stored in your path file stored in the `api` variable.
---
+++ returnActiveLoans.js
@@ -0,0 +1,22 @@
+// returns all active loans for all currencies
+const api = require('/Users/home/poloniex-api.js');
+const Poloniex = require('./lib/poloniex');
+const Table = require('cli-table');
+
+api.poloniex.returnActiveLoans(function(err, data) {
+  console.log("Poloniex Active Loans:");
+  let table = new Table({
+    head: ['id', 'rate', 'amount', 'duration', 'autoRenew', 'date', 'fees'],
+    colWidths: [11, 10, 16, 10, 11, 14, 17]
+  });
+  if (err) {
+    console.log("Error", err);
+    return;
+  }
+  for (let i = 0; i < data.provided.length; i++) {
+    table.push(
+      [data.provided[i].id, (data.provided[i].rate * 100).toFixed(4) + " %", data.provided[i].amount + " " + data.provided[i].currency, data.provided[i].duration + " days", data.provided[i].autoRenew, data.provided[i].date, data.provided[i].fees + " " + data.provided[i].currency]
+    )
+  }
+  console.log(table.toString());
+});
