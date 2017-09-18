c43f26328e1f7f0f8e167533c16230acfff02b10
Shaun Hoyes <jshoyes@gmail.com>
2017-09-17 20:03:04 -0600

    highlight BTC offer rate in orange

--- lendingRates/btc.js
+++ lendingRates/btc.js
@@ -5,5 +5,5 @@ const
 api.Poloniex.STRICT_SSL = false;
 
 api.poloniex.returnLoanOrders(currency, function(err, data) {
-  console.log(currency + ` Offer Rate: ` + (data.offers[10].rate * 100).toFixed(4) + "%");
+  console.log(api.clc.xterm(202)(currency + ` Offer Rate`) + `: ` + (data.offers[10].rate * 100).toFixed(4) + "%"); // BTC rate is highlighted in orange
 });
